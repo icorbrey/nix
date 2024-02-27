4b2ef17f240cbf45617e758a97149fa588ea42b8
Isaac Corbrey <isaac.corbrey@doitbest.com>
2024-02-27 15:20:10 -0500

    feat: Rebuild user

---
+++ rebuild-user.sh
@@ -0,0 +1,14 @@
+user=$1
+if [[ ! -f ./users/$user.nix ]]; then
+    echo "error: System \"$user\" does not exist."
+    exit
+fi
+
+vim "./users/$user.nix"
+alejandra . &>/dev/null
+git diff -U0 **/*.nix
+echo "Rebuilding "$user"..."
+home-manager --flake .#$user &> .nixos-switch.log \
+    || (cat .nixos-switch.log | grep --color error && false)
+current=$(home-manager generations 2>/dev/null | head -n 1 | awk '{print $5}')
+git commit -am "feat($user): Generation $current"
