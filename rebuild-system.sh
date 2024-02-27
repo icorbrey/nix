bee6cc2f09555977ef89b455f484576b38f29686
Isaac Corbrey <isaac.corbrey@doitbest.com>
2024-02-27 13:38:59 -0500

    feat: Add system rebuild script

---
+++ rebuild-system.sh
@@ -0,0 +1,14 @@
+system=$1
+if [[ ! -f ./systems/$system.txt ]]; then
+    echo "error: System \"$system\" does not exist."
+    exit
+fi
+
+vim "./systems/$system.nix"
+alejandra . &>/dev/null
+git diff -U0 *.nix
+echo "Rebuilding "$system"..."
+sudo nixos-rebuild switch --flake .#$system &> .nixos-switch.log \
+    || (cat .nixos-switch.log | grep --color error && false)
+current = $(nixos-rebuild list-generations | grep current)
+git commit -am "feat($system): Generation $current"
